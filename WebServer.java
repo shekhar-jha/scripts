77cd0cc056d5104fb0a12d0ace772c0844884ae0
Shekhar Jha <shekhar-jha@users.noreply.github.com>
2017-09-15 14:13:57 -0500

    Listen address & skip port
    1. Ability to listen on specific address
    2. Skip ports that are already being listened on
    3. separate usage method.

--- WebServer.java
+++ WebServer.java
@@ -7,6 +7,10 @@ import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.List;
+import java.util.ArrayList;
+import java.net.*;
+
 
 public class WebServer {
 
@@ -20,24 +24,56 @@ public class WebServer {
         }
     }
 
+	public static void usage(){
+        log("Usage: java WebServer [-addr <listenAddress>] <port1> <port2> ...");
+	}
     public static void main(String[] args) throws IOException {
         for (int i = 0; i < data.length; i++)
             data[i] = (byte) i;
         if (args.length == 0) {
-            log("Usage: java WebServer <port1> <port2> ...");
+			usage();
             return;
         }
+		boolean listenAddressProvided = false;
+		String listenAddress = "0.0.0.0";
+		String connectAddress = null;
+		if (args.length > 2 && args[0].startsWith("-add")) {
+			listenAddress = args[1];
+			connectAddress = args[1];
+			listenAddressProvided = true;
+		} else {
+			log("Using listen address as " + listenAddress);
+			connectAddress = java.net.InetAddress.getLocalHost().getHostName();
+			log("Using connection validation address as " + connectAddress);
+		}
+		List<Integer> applicablePorts = new ArrayList<Integer>();
+		for (int counter = listenAddressProvided?2:0; counter < args.length; counter++) {
+			String portValue = args[counter];
+			int port = -1;
+			try {
+				port = Integer.parseInt(portValue);
+				Socket sock = new Socket();
+				sock.connect(new InetSocketAddress(connectAddress, port), 2000);
+                sock.getOutputStream().write("h".getBytes());
+                sock.getInputStream().available();
+                sock.close();
+				log("Skipping port " + portValue + " since it is already being listened on.");
+			}catch(NumberFormatException exception) { 
+				log("Skipping port " + portValue + " since it is not a number.");
+			}catch (ConnectException exception) {
+				applicablePorts.add(port);
+			}
+		}
+		if (applicablePorts.size() == 0) {
+			log("Nothing to do since no ports are available to listen to");
+			return;
+		}
         Selector selector = Selector.open();
-        for (String portValue : args) {
-            try {
-                int port = Integer.parseInt(portValue);
-                ServerSocketChannel server = ServerSocketChannel.open();
-                server.configureBlocking(false);
-                server.socket().bind(new InetSocketAddress(port));
-                server.register(selector, SelectionKey.OP_ACCEPT);
-            } catch (NumberFormatException exception) {
-                log("Skipping invalid port " + portValue);
-            }
+        for (int portValue : applicablePorts) {
+			ServerSocketChannel server = ServerSocketChannel.open();
+			server.configureBlocking(false);
+			server.socket().bind(new InetSocketAddress(listenAddress, portValue));
+			server.register(selector, SelectionKey.OP_ACCEPT);
         }
 
         while (true) {
