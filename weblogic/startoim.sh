fed5edb35a80817ce87a180c8658186e0c17d8fd
Shekhar Jha <shekhar-jha@users.noreply.github.com>
2017-10-18 14:53:25 -0500

    Update startoim.sh

--- weblogic/startoim.sh
+++ weblogic/startoim.sh
@@ -1,10 +1,22 @@
-#/opt/fmw/oud/oim/OUD/bin/start-ds
-export MW_HOME=/opt/fmw/Middleware
-export DOMAIN_HOME=/opt/fmw/oim/domains/oim/
+#!/bin/bash
 
-cd /opt/fmw/tmp
-nohup $MW_HOME/wlserver_10.3/server/bin/startNodeManager.sh &
+source /home/oimsvc/bin/common.sh
+cd ${SCRIPT_TEMP_DIR}
 
-nohup $DOMAIN_HOME/bin/startWebLogic.sh &
-. $DOMAIN_HOME/bin/setDomainEnv.sh
-java weblogic.WLST /opt/fmw/oim/oimstart.py
+echo "###################################"
+echo "#### Starting Node Manager    #####"
+echo "###################################"
+nohup $MW_HOME/wlserver_10.3/server/bin/startNodeManager.sh >node.log &
+
+source $DOMAIN_HOME/bin/setDomainEnv.sh >> /dev/null
+
+if [ "${IS_ADMIN_SERVER}" == "TRUE" ];
+then
+    echo "###################################"
+    echo "#### Starting Admin Server    #####"
+    echo "###################################"
+    nohup $DOMAIN_HOME/bin/startWebLogic.sh > ${DOMAIN_HOME}/servers/AdminServer/logs/AdminServer.out &
+    $JAVA_HOME/bin/java -Dweblogic.security.SSL.enableJSSE=true -Dweblogic.security.SSL.minimumProtocolVersion=TLSv1.2 weblogic.WLST  -loadProperties ${SCRIPT_BASE_DIR}/config.properties ${SCRIPT_BASE_DIR}/oimstart.py `hostname`
+else
+    $JAVA_HOME/bin/java -Dweblogic.security.SSL.enableJSSE=true -Dweblogic.security.SSL.minimumProtocolVersion=TLSv1.2 weblogic.WLST  -loadProperties ${SCRIPT_BASE_DIR}/config.properties ${SCRIPT_BASE_DIR}/oimstart-nm.py `hostname`
+fi
